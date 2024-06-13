72f88b2d7b99caff7d80235d66e1b2dda0177371
Christian Frattini <chrifrat1@gmail.com>
2024-06-13 13:25:34 +0100

    nativewind css and assets/constants files & fonts

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { Link } from "expo-router";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>PORCHIDDIO</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-2xl font-pblack">Aora!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: "blue" }}>
         Go to Profile
@@ -13,12 +13,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
