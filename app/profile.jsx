dff63f9a377a608048d3f35b67147f0bb6140fcb
Christian Frattini <chrifrat1@gmail.com>
2024-06-13 12:38:19 +0100

    react native project setup

---
+++ app/profile.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
+
+const Profile = () => {
+  return (
+    <View>
+      <Text>Profile</Text>
+    </View>
+  );
+};
+
+export default Profile;
+
+const styles = StyleSheet.create({});
